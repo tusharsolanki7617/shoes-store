df27162caad18d55833b68e277685d4b016477c7
tushar solanki <162533122+tusharsolanki7617@users.noreply.github.com>
2026-04-22 13:13:19 +0530

    Add files via upload

---
+++ SQL/shoes_store.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 22, 2026 at 09:42 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shoes_store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+-- Error reading structure for table shoes_store.categories: #1142 - SHOW command denied to user ''@'localhost' for table `shoes_store`.`categories`
+-- Error reading data for table shoes_store.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shoes_store`.`categories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_messages`
+--
+-- Error reading structure for table shoes_store.contact_messages: #1142 - SHOW command denied to user ''@'localhost' for table `shoes_store`.`contact_messages`
+-- Error reading data for table shoes_store.contact_messages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shoes_store`.`contact_messages`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coupons`
+--
+-- Error reading structure for table shoes_store.coupons: #1142 - SHOW command denied to user ''@'localhost' for table `shoes_store`.`coupons`
+-- Error reading data for table shoes_store.coupons: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shoes_store`.`coupons`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+-- Error reading structure for table shoes_store.orders: #1142 - SHOW command denied to user ''@'localhost' for table `shoes_store`.`orders`
+-- Error reading data for table shoes_store.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shoes_store`.`orders`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+-- Error reading structure for table shoes_store.order_items: #1142 - SHOW command denied to user ''@'localhost' for table `shoes_store`.`order_items`
+-- Error reading data for table shoes_store.order_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shoes_store`.`order_items`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table shoes_store.products: #1142 - SHOW command denied to user ''@'localhost' for table `shoes_store`.`products`
+-- Error reading data for table shoes_store.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shoes_store`.`products`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+-- Error reading structure for table shoes_store.reviews: #1142 - SHOW command denied to user ''@'localhost' for table `shoes_store`.`reviews`
+-- Error reading data for table shoes_store.reviews: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shoes_store`.`reviews`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table shoes_store.users: #1142 - SHOW command denied to user ''@'localhost' for table `shoes_store`.`users`
+-- Error reading data for table shoes_store.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shoes_store`.`users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist`
+--
+-- Error reading structure for table shoes_store.wishlist: #1142 - SHOW command denied to user ''@'localhost' for table `shoes_store`.`wishlist`
+-- Error reading data for table shoes_store.wishlist: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `shoes_store`.`wishlist`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
